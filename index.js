87a3e17b568bfdbcf536dba2819abda521f86302
Babyhulk89 <131919459+Babyhulk89@users.noreply.github.com>
2023-11-04 12:56:58 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Note: This is not a good practice, but it's for demonstration purposes.
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; // Modifies the global variable.
+}
+
+
+const leastFavoriteCustomer = 'someone';
+
+
+function changeLeastFavoriteCustomer() {
+  // This will throw an error because you cannot reassign a constant.
+  leastFavoriteCustomer = 'new least favorite';
+}
